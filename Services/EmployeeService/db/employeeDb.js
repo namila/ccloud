80e1d564721b54eb7415eb648bfbce6b0b2063e8
Namila Perera <NamilaP@99x.lk>
2018-12-18 11:38:04 +0530

    employee service modified

--- Services/EmployeeService/db/employeeDb.js
+++ Services/EmployeeService/db/employeeDb.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-module.exports = mongoose.createConnection('mongodb://ccloud-employee-service-db:27017/employeeDb');
+module.exports = mongoose.createConnection('mongodb://ccloud-employee-db:27017/employeeDb');
 //module.exports = mongoose.createConnection('mongodb://localhost:27017/employeeDb');
