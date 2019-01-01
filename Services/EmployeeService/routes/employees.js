76215ddc038aba045ea727b24cd700b1d4b34b49
Namila Perera <NamilaP@99x.lk>
2019-01-01 22:22:11 +0530

    employee serivce modified

--- Services/EmployeeService/routes/employees.js
+++ Services/EmployeeService/routes/employees.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var uuid = require('uuid');
 //var Employee = require('../schemas/employeeSchema');
 var dynamoDbService = require('../db/dynamodb');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var users = [
-    {
-      Name: 'Namila3',
-      Age: 28
+  var params ={
+    TableName:"EmployeeTable"
+  }
+
+  dynamoDbService.scan(params, function(error, data){
+    if(error){
+      res.send({status: 'Failed'});
     }
-  ];
-  res.send(users);
+    else{
+      res.send(data);
+    }
+  });
 });
 
 router.post('/', function(req, res, next){
   var employeeObject = {
     TableName: "EmployeeTable",
     Item:{
-      "EmployeeId": {S: req.body.employeeId},
+      "EmployeeId": {S: uuid.v1()},
       "EmployeeName": {S: req.body.employeeName},
       "EmployeeAddress": {S: req.body.employeeAddress}
     }
